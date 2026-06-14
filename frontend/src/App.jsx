dcef6397e820e34a983ffcac759f703d87c9017b
Helika Athukoralla <hathukoralla@gmail.com>
2026-06-14 22:39:13 +0530

    final commit: Update API endpoint to Railway endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
     try
     {
-      const res = await axios.post("http://localhost:5000/summarize", formData, {
+      const res = await axios.post("https://ai-document-summarizer-production.up.railway.app/summarize", formData, {
         headers: {"Content-Type": "multipart/form-data"}
       })
       setSummary(res.data.summary)
